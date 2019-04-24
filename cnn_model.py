2eafdccfe937bcebf5f36d2de1eaf072a33bbc4b
lr12 <1533954499@qq.com>
2019-04-24 15:33:38 +0800

    ms

--- cnn_model.py
+++ cnn_model.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 class TCNNConfig(object):
     """CNN配置参数"""
 
-    embedding_dim = 300  # 词向量维度
+    embedding_dim = 64  # 词向量维度
     seq_length = 800  # 序列长度
     num_classes = 6 # 类别数
     num_filters = 256  # 卷积核数目
@@ -18,8 +18,8 @@ class TCNNConfig(object):
     dropout_keep_prob = 0.5  # dropout保留比例
     learning_rate = 1e-3  # 学习率
 
-    batch_size = 64  # 每批训练大小
-    num_epochs = 7 # 总迭代轮次
+    batch_size = 128  # 每批训练大小
+    num_epochs = 3 # 总迭代轮次
 
     print_per_batch = 20   # 每多少轮输出一次结果
     save_per_batch = 5  # 每多少轮存入tensorboard
