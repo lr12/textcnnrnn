8a314248da97775cb7fe3238f01d764f765a25d7
lr12 <1533954499@qq.com>
2019-04-30 15:17:21 +0800

    minshi

--- rnn_model.py
+++ rnn_model.py
@@ -9,7 +9,7 @@ class TRNNConfig(object):
     # 模型参数
     embedding_dim = 240      # 词向量维度
     seq_length = 600        # 序列长度
-    num_classes = 6        # 类别数
+    num_classes = 7        # 类别数
     vocab_size = 5000       # 词汇表达小
 
     num_layers= 2           # 隐藏层层数
